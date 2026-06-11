76f72f14b6efe9e51533a387e15a638adf5793cf
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-11 17:58:45 -0600

    P2: cluster2 - take upstream DeltaVectorMap.h (defines DeltaMapCommands enum) for DeltaAutoMap/DeltaAutoPackedMap

--- MMOCoreORB/src/server/zone/objects/scene/variables/DeltaVectorMap.h
+++ MMOCoreORB/src/server/zone/objects/scene/variables/DeltaVectorMap.h
@@ -9,7 +9,17 @@
 #define DELTAVECTORMAP_H_
 
 #include "engine/engine.h"
+#include "engine/util/json_utils.h"
 #include "server/zone/packets/DeltaMessage.h"
+
+class DeltaMapCommands {
+public:
+	enum Commands : uint8 {
+		ADD,
+		DROP,
+		SET
+	};
+};
 
 template <class K, class V> class DeltaVectorMap : public Serializable {
 protected:
@@ -41,19 +51,57 @@ public:
 		addSerializableVariables();
 	}
 
+	DeltaVectorMap& operator=(const DeltaVectorMap& ds) {
+		if (this == &ds) {
+			return *this;
+		}
+
+		updateCounter = ds.updateCounter;
+		vectorMap = ds.vectorMap;
+
+		return *this;
+	}
+
 	inline void addSerializableVariables() {
 		addSerializableVariable("vectorMap", &vectorMap);
 		addSerializableVariable("updateCounter", &updateCounter);
 	}
 
-	virtual int set(const K& key, const V& value, DeltaMessage* message = nullptr, int updates = 1) {
+	friend void to_json(nlohmann::json& j, const DeltaVectorMap<K, V>& map) {
+		j["vectorMap"] = map.vectorMap;
+		j["updateCounter"] = map.updateCounter;
+	}
+
+	virtual int add(const K& key, const V& value, DeltaMessage* message = nullptr, int updates = 1) {
 		int pos = vectorMap.put(key, value);
+
+		updateCounter += updates;
 
 		if (message != nullptr) {
 			if (updates != 0)
-				message->startList(updates, updateCounter += updates);
+				message->startList(updates, updateCounter);
 
-			message->insertByte(0);
+			message->insertByte(DeltaMapCommands::ADD);
+
+			K& nonconstK = const_cast<K&>(key);
+			V& nonconstV = const_cast<V&>(value);
+			TypeInfo<K>::toBinaryStream(&nonconstK, message);
+			TypeInfo<V>::toBinaryStream(&nonconstV, message);
+		}
+
+		return pos;
+	}
+
+	virtual int set(const K& key, const V& value, DeltaMessage* message = nullptr, int updates = 1) {
+		int pos = vectorMap.put(key, value);
+
+		updateCounter += updates;
+
+		if (message != nullptr) {
+			if (updates != 0)
+				message->startList(updates, updateCounter);
+
+			message->insertByte(DeltaMapCommands::SET);
 
 			K& nonconstK = const_cast<K&>(key);
 			V& nonconstV = const_cast<V&>(value);
@@ -68,15 +116,15 @@ public:
 		if (!vectorMap.contains(key))
 			return false;
 
-		V& value = vectorMap.get(key);
+		updateCounter += updates;
 
-		vectorMap.drop(key);
+		V& value = vectorMap.get(key);
 
 		if (message != nullptr) {
 			if (updates != 0)
-				message->startList(updates, updateCounter += updates);
+				message->startList(updates, updateCounter);
 
-			message->insertByte(1);
+			message->insertByte(DeltaMapCommands::DROP);
 
 			K& nonconstK = const_cast<K&>(key);
 			V& nonconstV = const_cast<V&>(value);
@@ -84,20 +132,23 @@ public:
 			TypeInfo<V>::toBinaryStream(&nonconstV, message);
 		}
 
+		vectorMap.drop(key);
+
 		return true;
 	}
 
-	virtual void insertToMessage(BaseMessage* msg) {
+	virtual void insertToMessage(BaseMessage* msg) const {
 		msg->insertInt(size());
 		msg->insertInt(getUpdateCounter());
 
 		for (int i = 0; i < size(); ++i) {
-			K& key = getKeyAt(i);
-			V& value = getValueAt(i);
+			const K& key = getKeyAt(i);
+			const V& value = getValueAt(i);
 
-			msg->insertByte(0);
-			TypeInfo<K>::toBinaryStream(&key, msg);
-			TypeInfo<V>::toBinaryStream(&value, msg);
+			msg->insertByte(DeltaMapCommands::ADD);
+
+			TypeInfo<K>::toBinaryStream(const_cast<K*>(&key), msg);
+			TypeInfo<V>::toBinaryStream(const_cast<V*>(&value), msg);
 		}
 	}
 
@@ -109,19 +160,31 @@ public:
 		return vectorMap.elementAt(index).getKey();
 	}
 
+	inline const V& getValueAt(int index) const {
+		return vectorMap.elementAt(index).getValue();
+	}
+
+	inline const K& getKeyAt(int index) const {
+		return vectorMap.elementAt(index).getKey();
+	}
+
 	inline V& get(const K& key) {
 		return vectorMap.get(key);
 	}
 
-	inline bool contains(const K& key) {
+	inline const V& get(const K& key) const {
+		return vectorMap.get(key);
+	}
+
+	inline bool contains(const K& key) const {
 		return vectorMap.contains(key);
 	}
 
-	inline int size() {
+	inline int size() const {
 		return vectorMap.size();
 	}
 
-	inline uint32 getUpdateCounter() {
+	inline uint32 getUpdateCounter() const {
 		return updateCounter;
 	}
 
