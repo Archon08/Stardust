a6ff9a8a1efab6fe20346167f1e62001ad4a46dd
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-11 13:38:08 -0600

    P1.3: PathFinderManager.h getFloorPath/addTriangleNodeEdges const FloorMesh + Vector<const Triangle*> mimicking upstream

--- MMOCoreORB/src/server/zone/managers/collision/PathFinderManager.h
+++ MMOCoreORB/src/server/zone/managers/collision/PathFinderManager.h
@@ -65,7 +65,7 @@ public:
 	 * @returns 0 when a successful path is found, the path is in nodes
 	 * @returns 1 when no successful path is found
 	 */
-	int getFloorPath(const Vector3& pointA, const Vector3& pointB, FloorMesh* floor, Vector<Triangle*>*& nodes);
+	int getFloorPath(const Vector3& pointA, const Vector3& pointB, const FloorMesh* floor, Vector<const Triangle*>*& nodes);
 	bool getRecastPath(const Vector3& start, const Vector3& end, NavArea* area, Vector<WorldCoordinates>* path, float& len, bool allowPartial);
 
 	Vector<WorldCoordinates>* findPathFromWorldToWorld(const WorldCoordinates& pointA, Vector<WorldCoordinates>& endPoints, Zone* zone, bool allowPartial);
@@ -78,7 +78,7 @@ protected:
 	Vector<WorldCoordinates>* findPathFromCellToCell(const WorldCoordinates& pointA, const WorldCoordinates& pointB);
 
 	Vector<WorldCoordinates>* findPathFromCellToDifferentCell(const WorldCoordinates& pointA, const WorldCoordinates& pointB);
-	void addTriangleNodeEdges(const Vector3& source, const Vector3& goal, Vector<Triangle*>* trianglePath, Vector<WorldCoordinates>* path, CellObject* cell);
+	void addTriangleNodeEdges(const Vector3& source, const Vector3& goal, Vector<const Triangle*>* trianglePath, Vector<WorldCoordinates>* path, CellObject* cell);
 
 	// The caller of this function is responsible for deleting the NavCollision objects.
 	// Collisions should be sorted from closest to farthest.
