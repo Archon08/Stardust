91bbd61a136223230fa24ef33c56bf2ef6b23350
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-11 13:25:43 -0600

    P1.3: LoginMessageProcessorTask.h ManagedReference->Reference for non-managed Message (engine3 _getObjectID)

--- MMOCoreORB/src/server/login/LoginMessageProcessorTask.h
+++ MMOCoreORB/src/server/login/LoginMessageProcessorTask.h
@@ -11,7 +11,7 @@ namespace server {
 	namespace login {
 	
 		class LoginMessageProcessorTask : public Task {
-			ManagedReference<Message*> message;
+			Reference<Message*> message;
 
 			LoginPacketHandler* packetHandler;
 
