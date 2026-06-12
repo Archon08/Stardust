15e833648d814a36b2aaf86ff057b51f34925ef9
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 02:31:26 -0600

    P2: const-qualify local (getter now returns const*) - ForceIntimidate2Command.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceIntimidate2Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceIntimidate2Command.h
@@ -63,7 +63,7 @@ public:
 				task->schedule(100);
 			}
 			if (!creature->checkCooldownRecovery(skillName)){
-														Time* timeRemaining = creature->getCooldownTime(skillName);
+														const Time* timeRemaining = creature->getCooldownTime(skillName);
 														creature->playMusicMessage("sound/ui_negative.snd");
 														creature->sendSystemMessage("Your target can not be snared with " + skillNameDisplay + " for another " +  getCooldownString(timeRemaining->miliDifference() * -1));
 										}
