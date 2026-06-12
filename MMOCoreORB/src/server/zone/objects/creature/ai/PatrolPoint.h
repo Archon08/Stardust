8855ade26e330d67a15db36bd813397559921c18
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-11 20:33:17 -0600

    P2: modern PatrolPoint.h

--- MMOCoreORB/src/server/zone/objects/creature/ai/PatrolPoint.h
+++ MMOCoreORB/src/server/zone/objects/creature/ai/PatrolPoint.h
@@ -6,6 +6,9 @@
 #define PATROLPOINT_H_
 
 #include "system/lang.h"
+
+#include "engine/util/json_utils.h"
+
 #include "server/zone/objects/scene/SceneObject.h"
 #include "server/zone/objects/cell/CellObject.h"
 #include "server/zone/objects/scene/WorldCoordinates.h"
@@ -14,8 +17,10 @@ class PatrolPoint : public Serializable {
 	WorldCoordinates position;
 
 	bool reached;
-	
+
 	SerializableTime estimatedTimeOfArrival;
+
+	float direction;
 
 public:
 	PatrolPoint() {
@@ -35,7 +40,7 @@ public:
 
 		addSerializableVariables();
 	}
-	
+
 	PatrolPoint(const PatrolPoint& point) : Object(), Serializable() {
 		position = point.position;
 
@@ -60,6 +65,8 @@ public:
 
 		reached = p.reached;
 
+		direction = p.direction;
+
 		estimatedTimeOfArrival = p.estimatedTimeOfArrival;
 
 		return *this;
@@ -74,6 +81,8 @@ public:
 
 		reached = p.reached;
 
+		direction = p.direction;
+
 		estimatedTimeOfArrival = p.estimatedTimeOfArrival;
 
 		return *this;
@@ -83,18 +92,26 @@ public:
 	inline void addSerializableVariables() {
 		addSerializableVariable("position", &position);
 		addSerializableVariable("reached", &reached);
+		addSerializableVariable("direction", &direction);
 		addSerializableVariable("estimatedTimeOfArrival", &estimatedTimeOfArrival);
+	}
+
+	friend void to_json(nlohmann::json& j, const PatrolPoint& p) {
+		j["position"] = p.position;
+		j["reached"] = p.reached;
+		j["direction"] = p.direction;
+		j["estimatedTimeOfArrival"] = p.estimatedTimeOfArrival;
 	}
 
 	Vector3 getWorldPosition() {
 		return position.getWorldPosition();
 	}
 
-	bool isInRange(SceneObject* obj, float range) {
+	virtual bool isInRange(SceneObject* obj, float range) {
 		Vector3 thisWorldPos = getWorldPosition();
 		Vector3 objWorldPos = obj->getWorldPosition();
 
-		return thisWorldPos.squaredDistanceTo(objWorldPos) <= (range * range);
+		return thisWorldPos.squaredDistanceTo(objWorldPos) < (range * range);
 	}
 
 	bool isInRange(PatrolPoint* obj, float range) {
@@ -104,35 +121,39 @@ public:
 		return thisWorldPos.squaredDistanceTo(objWorldPos) <= (range * range);
 	}
 
-	inline WorldCoordinates getCoordinates() {
+	inline const WorldCoordinates& getCoordinates() const {
 		return position;
 	}
 
 	//getters
-	inline float getPositionX() {
+	inline float getPositionX() const {
 		return position.getX();
 	}
-	
-	inline float getPositionY() {
+
+	inline float getPositionY() const {
 		return position.getY();
 	}
-	
-	inline float getPositionZ() {
+
+	inline float getPositionZ() const {
 		return position.getZ();
 	}
-	
-	inline CellObject* getCell() {
+
+	inline CellObject* getCell() const {
 		return position.getCell();
 	}
-	
+
+	inline float getDirection() const {
+		return direction;
+	}
+
 	inline Time* getEstimatedTimeOfArrival() {
 		return &estimatedTimeOfArrival;
 	}
 
-	inline bool isReached() {
+	inline bool isReached() const {
 		return reached;
 	}
-	
+
 	inline bool isPastTimeOfArrival() {
 		return estimatedTimeOfArrival.isPast() || estimatedTimeOfArrival.isPresent();
 	}
@@ -141,23 +162,27 @@ public:
 	inline void setPosition(float x, float z, float y) {
 		position.setCoordinates(Vector3(x, y, z));
 	}
-	
+
 	inline void setPositionX(float x) {
 		position.setX(x);
 	}
-	
+
 	inline void setPositionZ(float z) {
 		position.setZ(z);
 	}
-	
+
 	inline void setPositionY(float y) {
 		position.setY(y);
 	}
-	
+
 	inline void setCell(CellObject* cell) {
 		position.setCell(cell);
 	}
-	
+
+	inline void setDirection(float dir) {
+		direction = dir;
+	}
+
 	inline void setReached(bool value) {
 		reached = value;
 	}
@@ -170,7 +195,7 @@ public:
 	/**
 	 * Returns the string representation of the vector in (x, y, z) format plus the cellID.
 	 */
-	inline String toString() {
+	inline String toString() const {
 		return position.toString();
 	}
 };
