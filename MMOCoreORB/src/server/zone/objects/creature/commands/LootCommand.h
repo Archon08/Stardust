da0f0833183fed48d97ca8ff11bf6d3501dcd340
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 10:31:53 -0600

    P2: LootCommand const ContainerPermissions (getContainerPermissions now const-returning)

--- MMOCoreORB/src/server/zone/objects/creature/commands/LootCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/LootCommand.h
@@ -128,7 +128,7 @@ public:
 		int totalItems = lootContainer->getContainerObjectsSize();
 		if (totalItems < 1) return NOPICKUPITEMS;
 
-		ContainerPermissions* contPerms = lootContainer->getContainerPermissions();
+		const ContainerPermissions* contPerms = lootContainer->getContainerPermissions();
 		if (contPerms == nullptr) return NOPICKUPITEMS;
 
 		SceneObject* playerInventory = creature->getSlottedObject("inventory");
@@ -139,7 +139,7 @@ public:
 			SceneObject* object = lootContainer->getContainerObject(i);
 			if (object == nullptr) continue;
 
-			ContainerPermissions* itemPerms = object->getContainerPermissions();
+			const ContainerPermissions* itemPerms = object->getContainerPermissions();
 			if (itemPerms == nullptr) continue;
 
 			//Check if player owns the loot item.
