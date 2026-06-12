c0c2c645c7635719c911be4f817401189e8f918f
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-11 19:25:23 -0600

    P2: replace setCash/BankCredits with clear+addCash/BankCredits for starting credits in PlayerCreationManager.cpp

--- MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
+++ MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
@@ -419,8 +419,10 @@ bool PlayerCreationManager::createCharacter(ClientCreateCharacterCallback* callb
 	playerCreature->setClient(client);
 
 	// Set starting cash and starting bank
-	playerCreature->setCashCredits(startingCash, false);
-	playerCreature->setBankCredits(startingBank, false);
+	playerCreature->clearCashCredits(false);
+	playerCreature->clearBankCredits(false);
+	playerCreature->addCashCredits(startingCash, false);
+	playerCreature->addBankCredits(startingBank, false);
 
 	ManagedReference<PlayerObject*> ghost = playerCreature->getPlayerObject();
 
@@ -450,8 +452,10 @@ bool PlayerCreationManager::createCharacter(ClientCreateCharacterCallback* callb
 	}
 
 	// Set starting cash and starting bank
-	playerCreature->setCashCredits(startingCash, false);
-	playerCreature->setBankCredits(startingBank, false);
+	playerCreature->clearCashCredits(false);
+	playerCreature->clearBankCredits(false);
+	playerCreature->addCashCredits(startingCash, false);
+	playerCreature->addBankCredits(startingBank, false);
 
 	if (ghost != nullptr) {
 		int accID = client->getAccountID();
