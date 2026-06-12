8709cceacc06cd47b7dd005c2d4b0c0b940c34ad
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 01:46:01 -0600

    P2: ForceChokeCommand.h const-qualify local (upstream getter now returns const*)

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceChokeCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceChokeCommand.h
@@ -54,7 +54,7 @@ public:
 						// Setup debuff.
 
 						if (!creature->checkCooldownRecovery(skillName)){
-										Time* timeRemaining = creature->getCooldownTime(skillName);
+										const Time* timeRemaining = creature->getCooldownTime(skillName);
 										creature->playMusicMessage("sound/ui_negative.snd");
 										creature->sendSystemMessage("Your target can not be rooted with " + skillNameDisplay + " for another " +  getCooldownString(timeRemaining->miliDifference() * -1));
 						}
