7b96e10665070afb40f46297d7f571231cc8a4f6
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 02:35:20 -0600

    P2: MinefieldAttackTask include MinefieldDataComponent.h (referenced but not included; surfaced by upstream MinefieldZoneComponent take)

--- MMOCoreORB/src/server/zone/objects/installation/components/MinefieldAttackTask.h
+++ MMOCoreORB/src/server/zone/objects/installation/components/MinefieldAttackTask.h
@@ -13,6 +13,7 @@
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/packets/scene/PlayClientEffectLocMessage.h"
 #include "server/zone/objects/creature/commands/CombatQueueCommand.h"
+#include "server/zone/objects/installation/components/MinefieldDataComponent.h"
 
 class MinefieldAttackTask : public Task {
 	ManagedReference<SceneObject*> sceneObject;
