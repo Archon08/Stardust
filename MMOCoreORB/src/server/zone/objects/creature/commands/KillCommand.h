3adc02cb73bf62fe4046c252d34fe0cf09ee09cc
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 02:34:15 -0600

    P2: KillCommand.h QuadTreeEntry->TreeEntry (engine rename) + getInRangeObjects Z arg

--- MMOCoreORB/src/server/zone/objects/creature/commands/KillCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/KillCommand.h
@@ -139,14 +139,14 @@ public:
 		//Deal area damage if specified
 		if (area) {
 			//Retrieve nearby objects
-			SortedVector<QuadTreeEntry*> closeObjects;
+			SortedVector<TreeEntry*> closeObjects;
 			Zone* zone = creature->getZone();
 
 			if (creature->getCloseObjects() == nullptr) {
 #ifdef COV_DEBUG
 				creature->info("Null closeobjects vector in KillCommand::doQueueCommand", true);
 #endif
-				zone->getInRangeObjects(creature->getPositionX(), creature->getPositionY(), range, &closeObjects, true);
+				zone->getInRangeObjects(creature->getPositionX(), creature->getPositionZ(), creature->getPositionY(), range, &closeObjects, true);
 			}
 			else {
 				CloseObjectsVector* closeVector = (CloseObjectsVector*) creature->getCloseObjects();
