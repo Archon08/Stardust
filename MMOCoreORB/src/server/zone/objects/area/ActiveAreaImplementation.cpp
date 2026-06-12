d8b26af8fe02d70aa8f2be583f45ef612ccd3b20
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-11 18:23:52 -0600

    P2: ActiveArea impl - native bodies for getAreaCenter/getWidth/getHeight/getRectangularDimensions/getCuboidDimensions

--- MMOCoreORB/src/server/zone/objects/area/ActiveAreaImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/area/ActiveAreaImplementation.cpp
@@ -8,6 +8,7 @@
 #include "server/zone/objects/area/ActiveArea.h"
 #include "events/ActiveAreaEvent.h"
 #include "server/zone/objects/area/areashapes/AreaShape.h"
+#include "server/zone/objects/area/areashapes/RectangularAreaShape.h"
 
 bool ActiveAreaImplementation::containsPoint(float px, float py, uint64 cellid) {
 	if (cellObjectID != 0 && cellObjectID != cellid)
@@ -100,4 +101,55 @@ void ActiveAreaImplementation::initializeChildObject(SceneObject* controllerObje
 	if (objectParent != nullptr && objectParent->isCellObject()) {
 		setCellObjectID(objectParent->getObjectID());
 	}
+}
+
+Vector3 ActiveAreaImplementation::getAreaCenter() const {
+	if (areaShape != nullptr)
+		return areaShape->getAreaCenter();
+	else
+		return getPosition();
+}
+
+float ActiveAreaImplementation::getWidth() const {
+	if (areaShape != nullptr) {
+		RectangularAreaShape* rect = dynamic_cast<RectangularAreaShape*>(areaShape.get());
+
+		if (rect != nullptr)
+			return rect->getWidth();
+	}
+
+	return 0.f;
+}
+
+float ActiveAreaImplementation::getHeight() const {
+	if (areaShape != nullptr) {
+		RectangularAreaShape* rect = dynamic_cast<RectangularAreaShape*>(areaShape.get());
+
+		if (rect != nullptr)
+			return rect->getHeight();
+	}
+
+	return 0.f;
+}
+
+Vector4 ActiveAreaImplementation::getRectangularDimensions() const {
+	Vector3 center = getAreaCenter();
+
+	float halfWidth = getWidth() * 0.5f;
+	float halfHeight = getHeight() * 0.5f;
+
+	if (halfWidth == 0.f && halfHeight == 0.f) {
+		float r = getRadius();
+		halfWidth = r;
+		halfHeight = r;
+	}
+
+	// x,y = lower-left; z,w = upper-right
+	return Vector4(center.getX() - halfWidth, center.getY() - halfHeight, center.getX() + halfWidth, center.getY() + halfHeight);
+}
+
+Vector3 ActiveAreaImplementation::getCuboidDimensions() const {
+	float r = getRadius();
+
+	return Vector3(r, r, r);
 }
