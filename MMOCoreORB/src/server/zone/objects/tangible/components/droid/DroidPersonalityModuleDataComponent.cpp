313fb2102c0e2213c54448e8c3bceceaa1cac07a
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 10:40:53 -0600

    P2: DroidPersonalityModule sendReactionChat nullptr object arg (signature gained SceneObject* first param)

--- MMOCoreORB/src/server/zone/objects/tangible/components/droid/DroidPersonalityModuleDataComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/droid/DroidPersonalityModuleDataComponent.cpp
@@ -88,7 +88,7 @@ void DroidPersonalityModuleDataComponent::onCall() {
 
 	droid->registerObserver(ObserverEventType::DEFENDERADDED, observer);
 
-	droid->sendReactionChat(ReactionManager::HI, ReactionManager::NICE, true);
+	droid->sendReactionChat(nullptr, ReactionManager::HI, ReactionManager::NICE, true);
 }
 
 void DroidPersonalityModuleDataComponent::onStore() {
@@ -106,7 +106,7 @@ void DroidPersonalityModuleDataComponent::onStore() {
 	Locker dlock(droid);
 
 	droid->dropObserver(ObserverEventType::DEFENDERADDED, observer);
-	droid->sendReactionChat(ReactionManager::BYE, ReactionManager::NICE, true);
+	droid->sendReactionChat(nullptr, ReactionManager::BYE, ReactionManager::NICE, true);
 }
 
 void DroidPersonalityModuleDataComponent::copy(BaseDroidModuleComponent* other) {
