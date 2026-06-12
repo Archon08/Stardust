296e07161b405f76cebc98d68ede190386c41165
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-11 21:50:05 -0600

    P2: take modern upstream CreatureObjectMessage4.h (delta-clean; const-correct getHAM/delta accessors)

--- MMOCoreORB/src/server/zone/packets/creature/CreatureObjectMessage4.h
+++ MMOCoreORB/src/server/zone/packets/creature/CreatureObjectMessage4.h
@@ -10,46 +10,60 @@
 
 class CreatureObjectMessage4 : public BaseLineMessage {
 public:
-	CreatureObjectMessage4(CreatureObject* creo)
-			: BaseLineMessage(creo->getObjectID(), 0x4352454F, 4, 0x0E) {
-		//
+	CreatureObjectMessage4(CreatureObject* creo) : BaseLineMessage(creo, 'CREO', 4, 0x0E) {
+		// Accelerations.
 		insertFloat(creo->getAccelerationMultiplierBase());
 		insertFloat(creo->getAccelerationMultiplierMod());
 
-		//Encumbrances
-		DeltaVector<int>* encumbrances = creo->getEncumbrances();
+		// Encumbrances.
+		const DeltaVector<int>* encumbrances = creo->getEncumbrances();
 		encumbrances->insertToMessage(this);
 
-		// skill mods
-		SkillModList* skillMods = creo->getSkillModList();
+		// Skill Mods.
+		const SkillModList* skillMods = creo->getSkillModList();
 		skillMods->insertToMessage(this);
 
-		//
+		// Speed Multipliers.
 		insertFloat(creo->getSpeedMultiplierBase());
 		insertFloat(creo->getSpeedMultiplierMod());
 
-		// listenToID
+		// Listen to ID (Entertainer)
 		insertLong(creo->getListenID());
 
+		// Run Speed.
 		insertFloat(creo->getRunSpeed());
 
-		insertFloat(1.00625f);
-
-		insertFloat(creo->getTerrainNegotiation());  // Terrain Negotiation
-
-		// turn radius
-		insertFloat(creo->getTurnScale());
-
-		insertFloat(creo->getWalkSpeed());
+		// Slope Mods - Terrain Negotiation
+		insertFloat(creo->getSlopeModAngle());
 		insertFloat(creo->getSlopeModPercent());
 
-		//
-		insertInt(0);
-		insertInt(0);
+		// Turn Scale
+		insertFloat(creo->getTurnScale());
+
+		// Walk Speed
+		insertFloat(creo->getWalkSpeed());
+
+		// Water Mod (Swimming)
+		insertFloat(creo->getWaterModPercent());
+
+		// Space Mission Objects List
+		const DeltaSet<uint64, uint64>* spaceMissionObjects = creo->getSpaceMissionObjects();
+
+		int listSize = spaceMissionObjects->size();
+
+		insertInt(listSize);
+		insertInt(spaceMissionObjects->getUpdateCounter()); // Update Counter
+
+		for (int i = 0; i < listSize; i++) {
+			auto key = spaceMissionObjects->getKeyAt(i);
+			auto value = spaceMissionObjects->getValueAt(i);
+
+			insertLong(key);	// Mission Owner ID
+			insertLong(value);	// Mission Object ID
+		}
 
 		setSize();
 	}
-
 };
 
 #endif /*CREATUREOBJECTMESSAGE4_H_*/
