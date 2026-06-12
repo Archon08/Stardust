3a0ab9fd94b2f014257d5014cb87892d21a89499
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 11:44:59 -0600

    P2: CreatureManagerImpl thread direction through spawnCreature(objCRC)/placeCreature -> setHomeLocation+setDirection (mirror upstream)

--- MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
@@ -348,7 +348,7 @@ CreatureObject* CreatureManagerImplementation::spawnCreatureAsEventMob(uint32 te
 	return creo;
 }
 
-CreatureObject* CreatureManagerImplementation::spawnCreature(uint32 templateCRC, uint32 objectCRC, float x, float z, float y, uint64 parentID, bool persistent) {
+CreatureObject* CreatureManagerImplementation::spawnCreature(uint32 templateCRC, uint32 objectCRC, float x, float z, float y, uint64 parentID, bool persistent, float direction) {
 	CreatureTemplate* creoTempl = creatureTemplateManager->getTemplate(templateCRC);
 
 	if (creoTempl == nullptr)
@@ -372,7 +372,7 @@ CreatureObject* CreatureManagerImplementation::spawnCreature(uint32 templateCRC,
 		error("could not spawn template " + templateToSpawn);
 	}
 
-	placeCreature(creature, x, z, y, parentID);
+	placeCreature(creature, x, z, y, parentID, direction);
 
 	return creature;
 }
@@ -421,7 +421,7 @@ CreatureObject* CreatureManagerImplementation::createCreature(uint32 templateCRC
 	return creature;
 }
 
-void CreatureManagerImplementation::placeCreature(CreatureObject* creature, float x, float z, float y, uint64 parentID) {
+void CreatureManagerImplementation::placeCreature(CreatureObject* creature, float x, float z, float y, uint64 parentID, float direction) {
 	if (creature == nullptr)
 		return;
 
@@ -435,10 +435,11 @@ void CreatureManagerImplementation::placeCreature(CreatureObject* creature, floa
 
 	if (creature->isAiAgent()) {
 		AiAgent* aio = cast<AiAgent*>(creature);
-		aio->setHomeLocation(x, z, y, cellParent);
+		aio->setHomeLocation(x, z, y, cellParent, direction);
 	}
 
 	creature->initializePosition(x, z, y);
+	creature->setDirection(Quaternion(Vector3(0, 1, 0), direction));
 
 	if (cellParent != nullptr) {
 		cellParent->transferObject(creature, -1);
