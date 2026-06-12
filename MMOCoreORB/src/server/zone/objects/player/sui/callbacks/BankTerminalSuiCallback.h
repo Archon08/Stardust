d9125eea85c854d7cb0b4e6836c71041942fc5eb
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 07:06:27 -0600

    P2: BankTerminalSuiCallback - setCashCredits/setBankCredits removed; use transferCredits(cash,bank) per modern CreditObject API

--- MMOCoreORB/src/server/zone/objects/player/sui/callbacks/BankTerminalSuiCallback.h
+++ MMOCoreORB/src/server/zone/objects/player/sui/callbacks/BankTerminalSuiCallback.h
@@ -48,8 +48,7 @@ public:
 		uint32 currentBank = player->getBankCredits();
 
 		if ((currentCash + currentBank) == ((uint32) cash + (uint32) bank)) {
-			player->setCashCredits(cash);
-			player->setBankCredits(bank);
+			player->transferCredits(cash, bank);
 		}
 
 		player->sendSystemMessage("@base_player:bank_success");
