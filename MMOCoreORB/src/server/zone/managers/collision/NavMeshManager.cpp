41e42e8ccd9b7d7da254164306929f2efb2628e2
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-11 18:26:13 -0600

    P2: rename QuadTreeEntry->TreeEntry in NavMeshManager.cpp

--- MMOCoreORB/src/server/zone/managers/collision/NavMeshManager.cpp
+++ MMOCoreORB/src/server/zone/managers/collision/NavMeshManager.cpp
@@ -154,7 +154,7 @@ void NavMeshManager::startJob(Reference<NavMeshJob*> job) {
 
 	info("Starting building navmesh for area: " + name + " on planet: " + zone->getZoneName() + " at: " + area->getPosition().toString(), true);
 
-	SortedVector <ManagedReference<QuadTreeEntry *>> closeObjects;
+	SortedVector <ManagedReference<TreeEntry *>> closeObjects;
     zone->getInRangeSolidObjects(center.getX(), center.getZ(), range, &closeObjects, true);
 
     Vector <Reference<MeshData *>> meshData;
