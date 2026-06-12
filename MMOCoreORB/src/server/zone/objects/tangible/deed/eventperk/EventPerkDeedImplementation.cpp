46681e123deacd4e57b827f5ba77a53a6580b94e
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 09:33:13 -0600

    P2: EventPerkDeed use getContainerPermissionsForUpdate (getContainerPermissions now returns const)

--- MMOCoreORB/src/server/zone/objects/tangible/deed/eventperk/EventPerkDeedImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/deed/eventperk/EventPerkDeedImplementation.cpp
@@ -278,7 +278,7 @@ void EventPerkDeedImplementation::parseChildObjects(SceneObject* parent) {
 		if (child != nullptr)	{
 			Locker cLock(child, parent);
 
-			ContainerPermissions* permissions = child->getContainerPermissions();
+			ContainerPermissions* permissions = child->getContainerPermissionsForUpdate();
 			permissions->setOwner(parent->getObjectID());
 			permissions->setInheritPermissionsFromParent(false);
 			permissions->setDefaultDenyPermission(ContainerPermissions::MOVECONTAINER);
