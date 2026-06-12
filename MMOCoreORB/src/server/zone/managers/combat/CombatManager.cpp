def3ad539d9a69852913a267c3701f00c766d937
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-11 19:38:25 -0600

    P2: sendReactionChat now takes (object, type) - pass attacker (HELP/HIT) / tano (HITTARGET) per upstream (surgical)

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -322,14 +322,14 @@ int CombatManager::doTargetCombatAction(CreatureObject* attacker, WeaponObject* 
 		}
 
 		if (help)
-			aiAgent->sendReactionChat(ReactionManager::HELP);
+			aiAgent->sendReactionChat(attacker, ReactionManager::HELP);
 		else
-			aiAgent->sendReactionChat(ReactionManager::HIT);
+			aiAgent->sendReactionChat(attacker, ReactionManager::HIT);
 	}
 
 	if (damage > 0 && attacker->isAiAgent()) {
 		AiAgent* aiAgent = cast<AiAgent*>(attacker);
-		aiAgent->sendReactionChat(ReactionManager::HITTARGET);
+		aiAgent->sendReactionChat(tano, ReactionManager::HITTARGET);
 	}
 
 	return damage;
