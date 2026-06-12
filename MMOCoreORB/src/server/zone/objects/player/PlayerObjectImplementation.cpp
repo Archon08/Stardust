627701a0c9a84c8d47f06afc46609e157a02c347
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 02:31:46 -0600

    P2: const-qualify SkillList* locals (getSkillList returns const*)

--- MMOCoreORB/src/server/zone/objects/player/PlayerObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/player/PlayerObjectImplementation.cpp
@@ -917,7 +917,7 @@ void PlayerObjectImplementation::removeSchematics(Vector<ManagedReference<DraftS
 	if(player == nullptr)
 		return;
 
-	SkillList* playerSkillBoxList = player->getSkillList();
+	const SkillList* playerSkillBoxList = player->getSkillList();
 
 	for(int i = 0; i < playerSkillBoxList->size(); ++i) {
 		Skill* skillBox = playerSkillBoxList->get(i);
@@ -2389,7 +2389,7 @@ int PlayerObjectImplementation::getSpentJediSkillPoints() {
 
 	int jediSkillPoints = 0;
 
-	SkillList* skillList = player->getSkillList();
+	const SkillList* skillList = player->getSkillList();
 
 	for(int i = 0; i < skillList->size(); ++i) {
 		Skill* jediSkill = skillList->get(i);
