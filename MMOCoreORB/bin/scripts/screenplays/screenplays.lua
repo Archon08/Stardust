7c54defeda739c2bcd6ba689539b83ff096ec2bd
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-12 22:42:42 -0600

    P4: register space screenplays - add includeFile(space/screenplays.lua) to screenplays.lua

--- MMOCoreORB/bin/scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -656,6 +656,7 @@ includeFile("racetracks/racetracks.lua")
 
 --Space
 includeFile("space/space_chassis_dealer_conv_handler.lua")
+includeFile("space/screenplays.lua")
 
 --Record Keepers
 includeFile("record_keepers/record_keepers.lua")
