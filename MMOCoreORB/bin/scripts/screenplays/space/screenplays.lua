181af37af04c1aa8fe0bd4dcc258bfff9b095cc3
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-06-13 19:28:02 -0600

    F3: register Vortex Squadron handler include

--- MMOCoreORB/bin/scripts/screenplays/space/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/space/screenplays.lua
@@ -129,9 +129,9 @@ includeFile("space/conversations/rebel/havoc_squadron/aqzowConvoHandler.lua")
 -- Vortex Squadron
 includeFile("space/squadrons/VortexSquadronScreenplay.lua")
 
+includeFile("space/conversations/rebel/vortex_squadron/v3fxConvoHandler.lua")
 --includeFile("space/conversations/rebel/vortex_squadron/evinConvoHandler.lua")
 --includeFile("space/conversations/rebel/vortex_squadron/ezkielConvoHandler.lua")
---includeFile("space/conversations/rebel/vortex_squadron/v3fxConvoHandler.lua")
 --includeFile("space/conversations/rebel/vortex_squadron/vrovelConvoHandler.lua")
 
 
